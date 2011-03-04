e03ca1f598a29a77e263cd7442acf0c1a89e18cf
Andrew Nicolaou <public@andrewnicolaou.co.uk>
2011-03-04 12:27:04 +0000

    Support for chrome.webNavigation as well as chrome.experimental.webNavigation

--- src/js/bootstrap.js
+++ src/js/bootstrap.js
@@ -11,30 +11,54 @@ var Bootstrap = (function (){
   
   function addAPI( object, events, handler ) {
     events.forEach( function (eventName) {
-        object[eventName].addListener(
-          handler
-        );
+        object[eventName].addListener( handler );
     });
   };
   
-  function registerListeners() {
-    addAPI( 
-      chrome.experimental.webNavigation, 
-      ['onDOMContentLoaded'], 
-      function ( evt ) {
-        // Get tab for this event and initialise
-        // controller
-        chrome.tabs.get( evt.tabId, function(tab) {
-          new Controller( evt.url, tab );
-        });
+  var STABLE        = 'STABLE',
+      EXPERIMENTAL  = 'EXPERIMENTAL';
+  
+  function getApiNs() {
+      var ns;
+      if (chrome.webNavigation) {
+        ns = {
+          type: STABLE,
+          ns  : chrome 
+        };
+      } else if (chrome.experimental && chrome.experimental.webNavigation) {
+        ns = {
+          type: EXPERIMENTAL,
+          ns  : chrome.experimental 
+        };        
       }
-    );    
+      return ns;
   }
+  
+  function registerListeners() {
+    var apiNs = getApiNs();
+    if ( apiNs ) {
+       addAPI( 
+          apiNs.ns.webNavigation, 
+          ['onDOMContentLoaded'], 
+          function ( evt ) {
+            // Get tab for this event and initialise
+            // controller
+            chrome.tabs.get( evt.tabId, function(tab) {
+              console.log('Creating new controller for ', evt.url, tab);
+              new Controller( evt.url, tab );
+            });
+          }
+        );      
+    } else {
+      console.error('webNavigation API doesn\'t exist. Exiting');
+    }
+    console.log('webNavigation API is ', apiNs.type);
+  };
   
   return {
     init: function () {
       registerListeners();
     }
-  }
+  };
   
 })();
