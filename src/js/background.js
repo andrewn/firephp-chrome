e4a6569fcb0b359b95f107a2706349586b950b83
Andrew Nicolaou <public@andrewnicolaou.co.uk>
2011-09-10 00:53:38 +0100

    Add FirePHP version header to outgoing requests

--- src/js/background.js
+++ src/js/background.js
@@ -1,5 +1,27 @@
 var logger   = new Logger();
 
+// Add the X-FirePHP-Version header to all requests
+var filter, 
+    extraInfoSpec = ["blocking", "requestHeaders"];
+chrome.experimental.webRequest.onBeforeSendHeaders.addListener(
+    function(details) {
+        var headers = details.requestHeaders;
+        headers.push({
+            name  : "X-FirePHP-Version",
+            value : "0.6"
+        });
+        console.log(details);
+        return {
+            requestHeaders: headers
+        }
+    }, 
+    /* RequestFilter */ filter, 
+    /* array of string */ extraInfoSpec
+);
+
+
+// The dev panel sends us interesting requests
+// via dev_tools.js
 chrome.extension.onRequest.addListener(
   function(request, sender, sendResponse) {
     var headersMap = {};
