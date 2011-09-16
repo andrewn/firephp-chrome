4a83545e60a01a6409b11b53987c77fe480d8318
Patrik Henningsson <patrik.henningsson@gmail.com>
2011-09-17 01:02:52 +0200

    Added better formatting of stacktrace details (one less click to expand)

--- src/js/bridge.js
+++ src/js/bridge.js
@@ -1,38 +1,46 @@
 (function(){
+  
+  var dirObject = function(o) {
+    for (key in o) {
+      if (o.hasOwnProperty(key)) {
+        console.log(key + ':', o[key]);
+      }
+    }
+  };
+  
+  var dirStacktrace = function(stacktrace) {
+    console.group('Stack trace');
+    stacktrace.forEach(function (trace, idx) {
+      console.groupCollapsed(idx + '. ' + shortPath(trace.file) + '(' + trace.line + '): ' + trace.class + trace.type + trace.function + '(' + joinArguments(trace.args) + ')');
+      dirObject(trace);
+      console.groupEnd();
+    });
+    console.groupEnd();
+  };
+  
+  var shortPath = function (path) {
+    return path.split('/').splice(-3).join('/');
+  };
+  
+  var joinArguments = function (args) {
+    var result = [];
+    args.forEach(function (arg) {
+      if (typeof arg === 'object') {
+        result.push(arg.__className);
+      } else {
+        result.push(arg);
+      }
+    });
+    return result.join(', ');
+  };
+
   chrome.extension.onRequest.addListener(
     function(request, sender, sendResponse) {
       request.forEach(
         function ( item ) {
           if (item.action === 'exception') {
-            var shortPath = function (path) {
-                  return path.split('/').splice(-3).join('/');
-                },
-                joinArguments = function (args) {
-                  var result = [];
-                
-                  args.forEach(function (arg) {
-                    if (typeof arg === 'object') {
-                      result.push(arg.__className);
-                    } else {
-                      result.push(arg);
-                    }
-                  });
-                  
-                  return result.join(', ');
-                },
-                title = "Exception '" + item.body.Class + "' with message '" + item.body.Message + "' in " + shortPath(item.body.File) + ':' + item.body.Line;
-                
-            console.error(title);
-
-            console.group('Stack trace');
-
-            item.body.Trace.forEach(function (trace, idx) {
-              console.groupCollapsed(idx + '. ' + shortPath(trace.file) + '(' + trace.line + '): ' + trace.class + trace.type + trace.function + '(' + joinArguments(trace.args) + ')');
-              console.log(trace);
-              console.groupEnd();
-            })
-            
-            console.groupEnd();
+            console.error("Exception '" + item.body.Class + "' with message '" + item.body.Message + "' in " + shortPath(item.body.File) + ':' + item.body.Line);
+            dirStacktrace(item.body.Trace);
           } else {
             console[item.action]( item.body );
           }
